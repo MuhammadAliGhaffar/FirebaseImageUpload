08c34cfd2117f977e199caed89bb4c789d263414
MuhammadAliGhaffar <alibrohi999@gmail.com>
2021-02-14 19:56:42 +0500

    PICK IMAGE FROM DEVICE ADDING GLIDE & PICASSO

--- app/src/main/java/com/example/firebaseimageupload/UploadImageFragment.java
+++ app/src/main/java/com/example/firebaseimageupload/UploadImageFragment.java
@@ -1,57 +1,42 @@
 package com.example.firebaseimageupload;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link UploadImageFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.bumptech.glide.Glide;
+
+import static android.app.Activity.RESULT_OK;
+
+
 public class UploadImageFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final int PICK_IMAGE_REQUEST = 1;//Any postive no
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private Button btn_choose_image, btn_upload;
+    private EditText edt_filename;
+    private ImageView imageView;
+    private ProgressBar progressBar;
+    private TextView txt_showUploads;
 
-    public UploadImageFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment UploadImageFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static UploadImageFragment newInstance(String param1, String param2) {
-        UploadImageFragment fragment = new UploadImageFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private Uri mImageUri;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
@@ -59,5 +44,50 @@ public class UploadImageFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_upload_image, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        init(view);
+    }
+
+    public void init(View view) {
+        btn_choose_image = view.findViewById(R.id.btn_choose_image);
+        btn_upload = view.findViewById(R.id.btn_upload);
+        edt_filename = view.findViewById(R.id.edt_filename);
+        imageView = view.findViewById(R.id.imageView);
+        progressBar = view.findViewById(R.id.progressBar);
+        txt_showUploads = view.findViewById(R.id.txt_showUploads);
+
+        btn_choose_image.setOnClickListener(v -> {
+            openFileChooser();
+        });
+
+        btn_upload.setOnClickListener(v -> {
+
+        });
+
+        txt_showUploads.setOnClickListener(v -> {
+
+        });
+
+    }
+
+    private void openFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null) {
+            mImageUri = data.getData();
+            Glide.with(this).load(mImageUri).into(imageView);
+        }
     }
 }
